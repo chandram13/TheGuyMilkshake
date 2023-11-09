e7061a907c5dc648f867ed9d7c05ce8506d31ecc
Marvish Subhas Chandra <mschandr@uci.edu>
2023-11-09 04:41:56 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,18 @@
 # Marvish Chandra
 
 # Milkshake randomizer based on the milkshake mixins from Five Guys
+
 import random
 
 def randmomizerMS():
-    mixinsDict = {"Chocolate": 40,"Strawberry": 20,"Banana": 30,"Peanut Butter": 90,"Salted Caramel": 45,"Whipped Cream": 20,"OREO Cookies": 65,"Bacon": 80,"Vanilla": 20,"OREO Creme": 90,"Double Stuf": 60,"Reese's Cups": 150}
+    mixins = ["Chocolate","Strawberry","Banana","Peanut Butter","Salted Caramel","Whipped Cream","OREO Cookies","Bacon","Vanilla","OREO Creme","Double Stuf","Reese's Cups"]
     # must be able to select at least two mixins
+    random.shuffle(mixins)
+    print(mixins[0])
+    print(mixins[1])
 
-    keys = mixinsDict()
-    random.shuffle(keys)
+randmomizerMS()
 
-    for mixin in keys:
-        print(mixin, mixinsDict[mixin])
 
 
 
